2a0b2f4f60bdca9cab341e9b535e144327a90606
avryux <avryux04@gmail.com>
2023-09-08 14:41:48 -0300

    PostZapa Done

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ const upload = multer({ storage });
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, //Contraseña para el cole
-  //password: process.env.DB_PASSWORD2, 
+  //password: process.env.DB_PASSWORD, //Contraseña para el cole
+  password: process.env.DB_PASSWORD2, 
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
