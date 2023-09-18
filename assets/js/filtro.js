72f0b59fe7ad0e3b5fdca0c65079b0c8c1630dd6
jj <113184008+JuanMLeoni@users.noreply.github.com>
2023-09-18 10:07:25 -0300

    CSS filtro 18/09/23

--- assets/js/filtro.js
+++ assets/js/filtro.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     function filtrarPublicaciones() {
-        const talle = document.getElementById('talle').value;
+        const talle = document.querySelector(".selected").value;
         const marca = document.getElementById('marca').value;
         const origen = document.getElementById('pais').value;
         const genero = document.getElementById('genero').value;
         const pie = document.getElementById('pie').value;
         const precioMin = document.getElementById('precio-min').value;
         const precioMax = document.getElementById('precio-max').value;
-    
+
+
+
+
+
+
         // Realiza una nueva solicitud GET a la ruta de filtrado con los parámetros de los filtros
         fetch(`/filtrar-publicaciones?talle=${talle}&marca=${marca}&origen=${origen}&genero=${genero}&pie=${pie}&precioMin=${precioMin}&precioMax=${precioMax}`)
            .then(response => response.json())
@@ -36,13 +41,28 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Agrega un evento de cambio a cada filtro para que se ejecute la función de filtrado
-    document.getElementById('talle').addEventListener('change', filtrarPublicaciones);
+    //document.getElementById('talle').addEventListener('change', filtrarPublicaciones);
     document.getElementById('marca').addEventListener('change', filtrarPublicaciones);
     document.getElementById('pais').addEventListener('change', filtrarPublicaciones);
     document.getElementById('genero').addEventListener('change', filtrarPublicaciones);
     document.getElementById('pie').addEventListener('change', filtrarPublicaciones);
     document.getElementById('precio-min').addEventListener('change', filtrarPublicaciones);
     document.getElementById('precio-max').addEventListener('change', filtrarPublicaciones);
+    const botonestalle = document.querySelectorAll(".boton-talle")
+    botonestalle.forEach(function(boton){
+        boton.addEventListener("click", ()=> {
+            botonestalle.forEach(function(boton){
+                boton.classList.remove("selected")
+
+            })
+            boton.classList.add("selected")
+            let talle = boton.value
+            filtrarPublicaciones()
+
+    
+    
+        })
+    })
 });
 
 function crearTarjeta(publicacion) {
